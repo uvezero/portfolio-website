624a09f1c770d782748b592223fe1f83d77d0bb5
uvezero <juanlopezriosdecastro@gmail.com>
2024-10-08 12:27:28 +0200

    Minor changes to portfolio

--- home.py
+++ home.py
@@ -347,7 +347,7 @@ def projects():
         </a>
     </div>
     """, unsafe_allow_html=True)
-    
+    st write(" Web-app coming soon...")
     st.markdown('<div class="spacer"></div>', unsafe_allow_html=True)
 
     
@@ -411,7 +411,7 @@ def projects():
         </a>
     </div>
     """, unsafe_allow_html=True)
-
+    st.write("Git repo coming soon...")
 
 
 
